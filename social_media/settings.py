78c1f540c644631fcf58f649524fa32b0fbafdae
Anderson Portes <portsanderson@gmail.com>
2022-09-24 18:48:12 -0300

    Changing the settings.py

--- social_media/settings.py
+++ social_media/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+DEBUG = False
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
